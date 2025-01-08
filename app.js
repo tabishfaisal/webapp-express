051a3a537b4d8f6bb69f071e2c87f0464c49fc84
“tabihsfaisal“ <tabish.faisaltn@gmail.com>
2025-01-08 15:23:38 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+
+
+app.listen(PORT,()=>{
+    console.log(`server is running on ${PORT}`);
+})
